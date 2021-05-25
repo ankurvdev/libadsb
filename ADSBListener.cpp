74bd07e065d5ad9d07d011de9b32026a6643979c
ankurv <ankurv@microsoft.com>
2021-05-24 23:55:20 -0700

    Fix windows builds and update interfaces

--- ADSBListener.cpp
+++ ADSBListener.cpp
@@ -1,6 +1,12 @@
 #include "ADSBListener.h"
 
 #include <stdexcept>
+#include <thread>
+
+extern "C"
+{
+#include "dump1090.h"
+}
 
 // TODO : Thread safety
 static ADSB::IListener* singletonListener = nullptr;
@@ -16,29 +22,59 @@ struct ModeMessageImpl : ADSB::IModeMessage
 struct AirCraftImpl : ADSB::IAirCraft
 {
     AirCraftImpl(struct aircraft* a) {}
+
+    virtual uint32_t         MessageCount() const override { return 0; }
+    virtual uint32_t         Addr() const override { return _a->addr; }
+    virtual std::string_view FlightNumber() const override { return _a->flight; }
+    virtual time_point       LastSeen() const override { return time_point::clock::from_time_t(_a->seen); }
+    virtual uint32_t         SquakCode() const override { return _a->modeA; }
+    virtual int32_t          Altitude() const override { return _a->altitude; }
+    virtual uint32_t         Speed() const override { return _a->speed; }
+    virtual uint32_t         Heading() const override { return _a->track; }
+    virtual int32_t          Climb() const override { return _a->vert_rate; }
+    virtual int32_t          Lat1E7() const override { return static_cast<int32_t>(_a->lat * 10000000.0); }
+    virtual int32_t          Lon1E7() const override { return static_cast<int32_t>(_a->lon * 10000000.0); }
+
+    struct aircraft* _a;
 };
 
-void ADSB::IListener::Start()
+struct Dum1090DataProvider : ADSB::IDataProvider
 {
-    if (singletonListener != nullptr)
+    Dum1090DataProvider(std::string_view const& deviceName) : _deviceName(deviceName) {}
+
+    virtual void Start(ADSB::IListener& listener) override
     {
-        std::logic_error("Unfortunately we can only have a single listener in a process");
+        if (singletonListener != nullptr)
+        {
+            std::logic_error("Unfortunately we can only have a single listener in a process");
+        }
+
+        singletonListener = &listener;
+
+        _thrd = std::thread([&]() { startlistener(_deviceName.c_str()); });
     }
 
-    singletonListener = this;
-    startlistener("0");
-}
+    virtual void Stop() override
+    {
+        stoplistener();
+        _thrd.join();
+        singletonListener = nullptr;
+    }
 
-void ADSB::IListener::Stop()
-{
-    stoplistener();
-}
+    std::thread _thrd;
+    std::string _deviceName;
+};
 
-extern "C" void modesQueueOutput(struct modesMessage* mm, struct aircraft* a)
+extern "C" void modesQueueOutput(struct modesMessage* mm)
 {
-    singletonListener->OnMessage(ModeMessageImpl(mm), AirCraftImpl(a));
+    singletonListener->OnMessage(ModeMessageImpl(mm), AirCraftImpl(interactiveFindAircraft(mm->addr)));
 }
 
 extern "C" void modesSendAllClients(int service, void* msg, int len)
 {
+}
+
+std::unique_ptr<ADSB::IDataProvider> ADSB::CreateDump1090Provider(std::string_view const& deviceName)
+{
+    return std::make_unique<Dum1090DataProvider>(deviceName);
 }
--- ADSBListener.h
+++ ADSBListener.h
@@ -15,20 +15,35 @@ struct IModeMessage
 
 struct IAirCraft
 {
-    virtual ~IAirCraft() = default;
+    using time_point = std::chrono::time_point<std::chrono::system_clock>;
+
+    virtual ~IAirCraft()                          = default;
+    virtual uint32_t         MessageCount() const = 0;
+    virtual uint32_t         Addr() const         = 0;
+    virtual std::string_view FlightNumber() const = 0;
+    virtual time_point       LastSeen() const     = 0;
+    virtual uint32_t         SquakCode() const    = 0;
+    virtual int32_t          Altitude() const     = 0;
+    virtual uint32_t         Speed() const        = 0;
+    virtual uint32_t         Heading() const      = 0;
+    virtual int32_t          Climb() const        = 0;
+    virtual int32_t          Lat1E7() const       = 0;
+    virtual int32_t          Lon1E7() const       = 0;
 };
 
 struct IListener
 {
-    IListener(std::string_view const& deviceName) : _deviceName(deviceName) {}
-    virtual ~IListener() = default;
-
+    virtual ~IListener()                                          = default;
     virtual void OnMessage(IModeMessage const&, IAirCraft const&) = 0;
-
-    void Start();
-    void Stop();
-
-    private:
-    std::string _deviceName;
 };
+
+struct IDataProvider
+{
+    virtual ~IDataProvider() = default;
+
+    virtual void Start(IListener& listener) = 0;
+    virtual void Stop()                     = 0;
+};
+
+std::unique_ptr<IDataProvider> CreateDump1090Provider(std::string_view const& deviceName);
 }    // namespace ADSB
